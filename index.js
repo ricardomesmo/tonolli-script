838c0102540dac85b1d3b219a1c1d02364ba2266
ricardomesmo <macbook@MacBook-Pro-de-ricardmesmo.local>
2021-05-17 23:09:13 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var productName = document.getElementsByClassName("product-details__name");			
+if (productName[0].innerText === "Colchão de Mola Firm 34 iMassage") {
+    var labelName = document.getElementsByClassName("product-details__name");
+    labelName[0].insertAdjacentHTML('afterend', '<h3 style="display: inline-block; margin: .35714rem 0; color: var(--sale-label-color); background-color: var(--sale-label-background-color); padding: .28571rem .57143rem; border-radius: 1.78571rem; font-weight: 400; font-size: 1rem; margin-right: -.25em;">Tenha uma experiência incrível, clique no ícone RA a seguir e veja!</h3>')
+    var variant000 = document.getElementById("label_valor2_var000");
+    variant000.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/20798327-ebdd-4eee-9bf0-c53bf42450dd" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant001 = document.getElementById("label_valor2_var001");
+    variant001.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/b30d7839-d923-426d-865b-a1e6bacef182" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant002 = document.getElementById("label_valor2_var002");
+    variant002.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/c5deb52e-95ef-4896-8b52-ed8ec521a350" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant003 = document.getElementById("label_valor2_var003");
+    variant003.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/d1ad8bb7-505a-48a5-8ba3-370a7a31778e" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant004 = document.getElementById("label_valor2_var004");
+    variant004.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/4d3b9b8b-4bcd-4e5d-bb12-de82ebf0c2d1" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant005 = document.getElementById("label_valor2_var005");
+    variant005.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/738b9741-f8f1-4399-81af-12bd109f7e3d" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant006 = document.getElementById("label_valor2_var006");
+    variant006.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/eae31d8d-78eb-4bcd-86a6-df823fbda438" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant007 = document.getElementById("label_valor2_var007");
+    variant007.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/6982cf19-9aaf-42e7-9212-b9530670069c" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+} 
+if (productName[0].innerText === "Colchão de Mola Comfort 34") {
+    var labelName = document.getElementsByClassName("product-details__name");
+    labelName[0].insertAdjacentHTML('afterend', '<h3 style="display: inline-block; margin: .35714rem 0; color: var(--sale-label-color); background-color: var(--sale-label-background-color); padding: .28571rem .57143rem; border-radius: 1.78571rem; font-weight: 400; font-size: 1rem; margin-right: -.25em;">Tenha uma experiência incrível, clique no ícone RA a seguir e veja!</h3>')
+    var variant000 = document.getElementById("label_valor2_var000");
+    variant000.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/044123f4-beef-4ea3-8db2-2a408d6bc5f0" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant001 = document.getElementById("label_valor2_var001");
+    variant001.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/c8c4d986-c75d-4f94-abcd-ddb0ca01f2fc" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant002 = document.getElementById("label_valor2_var002");
+    variant002.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/74639dd3-225c-45b3-af43-f7399e4f8a05" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant003 = document.getElementById("label_valor2_var003");
+    variant003.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/d2e11a4f-9a59-4a6a-b23d-93dfc35f2542" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant004 = document.getElementById("label_valor2_var004");
+    variant004.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/25578250-6ffd-42f3-a0e9-291fde32d66b" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant005 = document.getElementById("label_valor2_var005");
+    variant005.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/66ba0b0e-1d42-49e7-a2d1-f52abfd770f3" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant006 = document.getElementById("label_valor2_var006");
+    variant006.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/e42ba195-dda0-4f62-b09e-6f9c9bd4bbc5" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+    var variant007 = document.getElementById("label_valor2_var007");
+    variant007.insertAdjacentHTML('afterend', '<a href="https://dashboardccoast.com/model/03004f7d-9f5d-482e-bc49-0761e1795ac2" target="_blank"><span>&nbsp;Realidade Aumentada&nbsp;</span></a>');
+}
