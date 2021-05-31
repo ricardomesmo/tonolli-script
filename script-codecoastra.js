59a25fdff5fb8fb6ea7c6d1fc6a5cc2d1aeb2600
ricardomesmo <macbook@MacBook-Pro-de-ricardmesmo.local>
2021-05-31 11:52:56 -0300

    correções links2

--- script-codecoastra.js
+++ script-codecoastra.js
@@ -1,5 +1,5 @@
 var productName = document.getElementsByClassName("product-details__name");			
-if (productName[0].innerText === "Colchão de Mola Firm 34 iMassage") {
+if (productName[0].innerText === "Colchão de Mola Firm 34") {
     var labelName = document.getElementsByClassName("product-details__name");
     labelName[0].insertAdjacentHTML('afterend', '<a href="https://www.dashboardccoast.com/model/738b9741-f8f1-4399-81af-12bd109f7e3d" target="_blank"><div style="display: block; height: auto; width: auto;"><h3 style="text-transform: uppercase; display: flex; justify-content: center; align-items: center; width: 250px; margin: .35714rem 0; color: #FFFFFF; background-color: #ff6f2f; padding: .75rem 2rem; border-radius: 1.78571rem; font-weight: 400; font-size: 1rem;">Veja no seu Quarto</h3><span>Tenha uma experiência incrível.<br />Clique para ver em realidade aumentada.</span></div></a>')
 }
